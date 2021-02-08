a3042789726197a460703a621c914d1eabbe0f93
unknown <niambaust17@gmail.com>
2021-02-08 12:18:03 +0600

    display lyrics to the user and show error message if someting wrong

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function searchSong()
+const searchSong = async () =>
 {
     const searchText = document.getElementById('song-name').value;
-    fetch(`https://api.lyrics.ovh/suggest/:${ searchText }`)
-        .then(res => res.json())
-        .then(data => showSongs(data.data))
-
+    const res = await fetch(`https://api.lyrics.ovh/suggest/:${ searchText }`);
+    const data = await res.json();
+    showSongs(data.data);
 };
 
 const showSongs = songs =>
 {
     const divContainer = document.getElementById('song-container');
+    divContainer.innerHTML = '';
     songs.forEach(song =>
     {
         const divSong = document.createElement('div');
@@ -18,11 +18,49 @@ const showSongs = songs =>
             <div class="col-md-9">
                 <h3 class="lyrics-name">${ song.title }</h3>
                 <p class="author lead">Album by <span>${ song.artist.name }</span></p>
+                <audio controls>
+                <source src="${ song.preview }" type="audio/ogg">
+                </audio>
             </div>
             <div class="col-md-3 text-md-right text-center">
-                <button class="btn btn-success">Get Lyrics</button>
+                <button onclick="getLyrics('${ song.artist.name }', '${ song.title }')" class="btn btn-success">Get Lyrics</button>
             </div>
         `;
         divContainer.appendChild(divSong);
     });
 };
+
+const getLyrics = async (artist, title) =>
+{
+    const res = await fetch(`https://api.lyrics.ovh/v1/${ artist }/${ title }`);
+    try
+    {
+        const data = await res.json();
+        showLyrics(data);
+    }
+    catch (error)
+    {
+        showError('Try Again Later')
+    }
+
+};
+
+// const getLyrics = (artist, title) =>
+// {
+//     fetch(`https://api.lyrics.ovh/v1/${ artist }/${ title }`)
+//         .then(res => res.json())
+//         .then(data => showLyrics(data))
+//         .catch(error => showError('Try Again Later'))
+// };
+
+const showLyrics = lyrics =>
+{
+    const lyricsDiv = document.querySelector('.single-lyrics');
+    const songLyrics = lyrics.lyrics;
+    lyricsDiv.innerText = songLyrics;
+}
+
+const showError = error =>
+{
+    document.getElementById('error-msg').innerText = error;
+}
