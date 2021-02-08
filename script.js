3f28c7126884d0770ca25ece4136e2c9c74da63f
unknown <niambaust17@gmail.com>
2021-02-08 20:32:42 +0600

    validation added

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
+const inputSong = document.getElementById('song-name');
 const searchSong = async () =>
 {
     const searchText = document.getElementById('song-name').value;
     const res = await fetch(`https://api.lyrics.ovh/suggest/:${ searchText }`);
     const data = await res.json();
     showSongs(data.data);
+    inputSong.value = '';
+    document.querySelector('.single-lyrics').style.display = 'none';
 };
 
 const showSongs = songs =>
 {
-    const divContainer = document.getElementById('song-container');
-    divContainer.innerHTML = '';
-    songs.forEach(song =>
+    if (inputSong.value.length != 0 && isNaN(inputSong.value))
     {
-        const newSong = song.preview.slice(39, song.preview.length);
-        // console.log(newSong);
-        // const newSong = 'c-e54b20c75f39a478f54e78b9b0796bc5-2.mp3';
-        const divSong = document.createElement('div');
-        divSong.className = 'single-result row align-items-center my-3 p-3';
-        divSong.innerHTML = `
+        const divContainer = document.getElementById('song-container');
+        divContainer.innerHTML = '';
+        songs.forEach(song =>
+        {
+            const newSong = song.preview.slice(39, song.preview.length);
+            // console.log(newSong);
+            // const newSong = 'c-e54b20c75f39a478f54e78b9b0796bc5-2.mp3';
+            const divSong = document.createElement('div');
+            divSong.className = 'single-result row align-items-center my-3 p-3';
+            divSong.innerHTML = `
             <div class="col-md-9">
                 <h3 class="lyrics-name">${ song.title }</h3>
                 <p class="author lead">Album by <span>${ song.artist.name }</span></p>
@@ -29,8 +34,9 @@ const showSongs = songs =>
                 <button onclick="getLyrics('${ song.artist.name }', '${ song.title }')" class="btn btn-success">Get Lyrics</button>
             </div>
         `;
-        divContainer.appendChild(divSong);
-    });
+            divContainer.appendChild(divSong);
+        });
+    }
 };
 
 const getLyrics = async (artist, title) =>
@@ -38,6 +44,7 @@ const getLyrics = async (artist, title) =>
     const res = await fetch(`https://api.lyrics.ovh/v1/${ artist }/${ title }`);
     try
     {
+        document.querySelector('.single-lyrics').style.display = 'block';
         const data = await res.json();
         showLyrics(data);
     }
@@ -58,9 +65,16 @@ const getLyrics = async (artist, title) =>
 
 const showLyrics = lyrics =>
 {
-    const lyricsDiv = document.querySelector('.single-lyrics');
-    const songLyrics = lyrics.lyrics;
-    lyricsDiv.innerText = songLyrics;
+    if (lyrics.lyrics.length != 0)
+    {
+        const lyricsDiv = document.querySelector('.single-lyrics');
+        const songLyrics = lyrics.lyrics;
+        lyricsDiv.innerText = songLyrics;
+    }
+    else
+    {
+        document.querySelector('.single-lyrics').style.display = 'none';
+    }
 }
 
 const showError = error =>
